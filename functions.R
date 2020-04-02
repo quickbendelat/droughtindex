201c0a2b8861511f5471e748f1405d0b34727012
Ben Gin <gin.benjamin@gmail.com>
2020-04-02 22:00:43 +1300

    auto file load * rename `create_snapshots()` function for old rmd file to still work * create new `create_snapthots()` to work with new rmd file that will read all nc file names and then apply the new function to load data then process

--- functions.R
+++ functions.R
@@ -7,13 +7,13 @@ create_plots <- function(df) {
 }
 
 
-create_snapshots <- function(nz_plot, file_num) {
+create_snapshots_old <- function(nz_plot, file_num) {
   plot_gg(nz_plot,multicore=TRUE,width=5,height=5,scale=250,windowsize=c(1132,700),
           zoom = 0.55, phi = 30)
   
   Sys.sleep(60)
   
-  render_snapshot(filename = sprintf("timeseries/nzdi%03d.png", file_num))
+  render_snapshot(filename = sprintf("timeseries/nzdi%03d.png", file_num), title_text = date_title)
   
   snapshottaken = TRUE
   
@@ -23,6 +23,38 @@ create_snapshots <- function(nz_plot, file_num) {
   gc()
   
   return(snapshottaken)
+}
+
+
+create_snapshots <- function(file, plot_title) {
+  
+  nc_dat <- nc_open(file)
+  ndvi.array <- ncvar_get(nc_dat) 
+  nc_close(nc_dat) 
+  nc_df <- ndvi.array %>% reshape2::melt() %>% 
+    rename(x = Var2,
+           y = Var1) %>% 
+    mutate(value = ifelse(!is.nan(value), value, NA))
+  rm(ndvi.array)
+  
+  p1 <- create_plots(nc_df)
+  
+  rm(nc_df)
+
+  plot_gg(p1,multicore=TRUE,width=5,height=5,scale=250,windowsize=c(1132,700),
+          zoom = 0.55, phi = 30)
+  
+  Sys.sleep(60)
+  
+  render_snapshot(filename = paste0("timeseries/", plot_title, ".png"), title_text = plot_title)
+
+  if(rgl::rgl.cur() != 0) {
+    rgl::rgl.close()
+  }
+  gc()
+  
+  snapshottaken = TRUE
+  snapshottaken
 }
 
 
